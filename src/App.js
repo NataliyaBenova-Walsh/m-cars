8cfbf733418ebe4a5331f50587c29f18d3c166dd
NataliyaBenova-Walsh <nataliyawalsh@gmail.com>
2022-08-09 13:37:46 +0200

    Initializa React and connect to back4app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { Header } from './header/Header';
-import {Footer} from './Footer';
-import {Home} from './home/Home';
-import {Cars} from './cars/Cars';
+
+// Import Parse minified version
+import Parse from 'parse/dist/parse.min.js';
+
+
+const PARSE_APPLICATION_ID = '9nTwJRLz0Ksu1AR8mNWuEfQylbUeJp3ccmqLvzIM';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'DSR96IE9jJprksIIHFRRES8viMQvj0KprIW89ZMC';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
-      <Home />
-
-      <Cars />
-
-      <Footer/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/Footer.js
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-    return (
-        <div id="footer">
-	        <p>&copy; Untitled. All rights reserved. Images by <a href="http://fotogrph.com/">Fotogrph</a>. Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
-        </div>
-    
-    );
-}
