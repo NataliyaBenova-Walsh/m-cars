30ae91babf394dab1169262a4ebf6b311982fbd6
NataliyaBenova-Walsh <110407899+NataliyaBenova-Walsh@users.noreply.github.com>
2022-08-11 00:47:31 +0200

    Added hosting

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
 		<>
 		<section className="hero">
 		<div className="heroContainer">
-			<h1>Rent local cars <span className="highlighted">from your community</span></h1>
+			<h1>Rent cars <span className="highlighted">from your community</span></h1>
 			<p>Instantly search for and book vehicles from trusted owners</p>
 		</div>
 		<div className="heroContainer">
@@ -24,7 +24,7 @@ export const Home = () => {
 					<h2 className="title"><a href="/">Welcome to Mcars </a></h2>
 					<div className="entry">
 						<p><a href="/"><img src={image2} width="600" height="170" alt="" /></a></p>
-                    	<p>Why rent with <strong>M Cars</strong>, a free platform that makes car sharing easier than ever</p>
+                    	<p>Why rent with <strong>M Cars</strong>?MCars is a free platform that makes car sharing easier than ever</p>
 						<p>The Mcars app makes it easy to discover, book and unlock affordable vehicles from owners in your area. Take your pick from dazzling electric city cars to family SUVs with plenty of storage. Skip the counter today and join the car sharing revolution.</p>
                     
 					
