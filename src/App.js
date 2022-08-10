3118c83f7f5fb23a81d6a5a35c55fc0d457b366c
NataliyaBenova-Walsh <110407899+NataliyaBenova-Walsh@users.noreply.github.com>
2022-08-10 22:11:31 +0200

    Added firebase db, fix some issues with the pictures

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 
 import './App.css';
-import './components/login/Login.css'
+import './components/login/Login.css';
+import './components/register/Register.css';
+import './components/create/Create.css';
 import { Routes, Route } from 'react-router-dom'
 
-// Import Parse minified version
-import Parse from 'parse/dist/parse.min.js';
+
+
 
 import { Header} from './components/header/Header';
 import { Home } from './components/home/Home';
-import { Cars } from './components/cars/Cars';
+
 import { Login } from './components/login/Login';
+import { Register } from './components/register/Register';
+import { Create } from './components/create/Create';
+import { Catalog } from './components/catalog/Catalog';
+import { useEffect, useState } from 'react';
+
+import { db } from './firebase-config';
+import {collection, getDocs} from 'firebase/firestore'
 
 
 
 
-const PARSE_APPLICATION_ID = '9nTwJRLz0Ksu1AR8mNWuEfQylbUeJp3ccmqLvzIM';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'DSR96IE9jJprksIIHFRRES8viMQvj0KprIW89ZMC';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollRef);
+      console.log(data.docs);
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      
+    }
+    getUsers();
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -29,12 +46,14 @@ function App() {
       
       <Routes>
         <Route path='/' element={<Home/>}> </Route>
-        <Route path='/login' element={<Login/>}></Route>
-        
+        <Route path='/login' element={<Login/>}> </Route>
+        <Route path='/register' element={<Register/>}> </Route>
+        <Route path='/create' element={<Create/>}> </Route>
+        <Route path='/catalog' element={<Catalog/>}> </Route>
       
-        </Routes>
+      </Routes>
      
-      <Cars/>
+     
     </div>
   );
 }
