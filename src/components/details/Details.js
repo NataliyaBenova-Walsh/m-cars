f7a5cc18c0694ba8d0b368dd447525601c331631
NataliyaBenova-Walsh <110407899+NataliyaBenova-Walsh@users.noreply.github.com>
2022-08-17 21:31:03 +0200

    Worked on Details page

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -4,6 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { getCarById } from '../services/CarServices';
 import { auth } from '../../firebase-config';
+import { getUserById, getUserByUid } from '../services/UserServices';
 
 
 
@@ -36,48 +37,57 @@ export const CarDetails = ({ car}) => {
          isOwner = true;
     }
     
-  
+    const [carOwner, setCarOwner] = useState({});
+
+    useEffect(() => {
+        const owner = async () => {
+        
+                const data = await getUserByUid(currentCar.ownerId);
+                console.log(data);
+            
+                setCarOwner(data);
+             
+        }
+        owner();  
+        
+    }, []);
+    
         
     
 
     return (
-    <div className='details-container'>
-        <div className="details-car-container">
-            <p><img src={currentCar.imgUrl} width="300" height="150" alt="" /></p>
-			<h2 className='car-model'>Model: {currentCar.carModel}</h2>
-			<h3 className='car-price'>Price from: {currentCar.price} </h3>
-            <h4 className='car-location'>Location: {currentCar.city}</h4>
-            <p className='cra-desc'>Description: {currentCar.desc}</p>
+        <>
+             <div className='details-container'>
+                <div className="details-car-container">
+                    <p><img src={currentCar.imgUrl} width="300" height="150" alt="" /></p>
+			        <h2 className='car-model'>Model: {currentCar.carModel}</h2>
+			        <h3 className='car-price'>Price from: {currentCar.price} </h3>
+                    <h4 className='car-location'>Location: {currentCar.city}</h4>
+                    <p className='car-desc'>Description: {currentCar.desc}</p>
 			
             {isOwner 
-            ? <div className="details-links">
-                <Link className='details-link' to={`/edit/${carId}`}>Edit</Link>
-                <Link className='details-link' to={`/delete/${carId}`}>Delete</Link>
-            </div>
-            : <p>You cannot edit or delete</p>
+                ? <div className="details-links">
+                    <Link className='details-link' to={`/edit/${carId}`}>Edit</Link>
+                    <Link className='details-link' to={`/delete/${carId}`}>Delete</Link>
+                </div>
+
+                : <div className='details-car-container owner-container'>
+                    <h2 className='owner-title'>Owner details</h2>
+                    <p className='owner-p'>Here are the owner details feel free to send an enquire for updated prices and availability</p>
+            
+                    <div className="contacts-owner">
+				        <div className='owner-card'>
+                            <h3 className='owner-name'>Name: </h3>
+                            <h3 className='owner-email'>Email: </h3>
+                            <a href='mailto: niqbenova@yahoo.com'>Email me</a>
+                        </div>
+                    </div>
+                    </div>
             }
 			
-		</div>
-
-        <div className='details-car-container owner-container'>
-            <h2 className='owner-title'>Owner details</h2>
-            <p className='owner-p'>Here are the owner details feel free to send an enquire for updated prices and availability</p>
-            
-            <div className="contacts-owner">
-				<div className='owner-card'>
-                    <h3 className='owner-name'>Name:</h3>
-                    <h3 className='owner-email'>Email: </h3>
-                    <a href='mailto: niqbenova@yahoo.com'>Email me</a>
-                </div>
-                  
-                    	
-            </div>
+		    </div>
         
-		</div>
-			
-             
-           
-        
-    </div>
+        </div>
+        </>
     );
 }
---
+++ src/components/services/UserServices.js
@@ -0,0 +1,36 @@
+import {db} from '../../firebase-config';
+import {collection, query, where, getDoc, doc, getDocs} from 'firebase/firestore';
+import { async } from '@firebase/util';
+
+
+export const getUserById = async (id) => {
+    const userRef = doc(db, "users", id);
+    const userSnap = await getDoc(userRef);
+    let userData = userSnap.data();
+    
+    if (userSnap.exists()) {
+    
+      console.log("user data:", userData);
+      return userData;
+    }
+    else {
+      // doc.data() will be undefined in this case
+      console.log("No such!");
+    }
+    }
+
+export const getUserByUid = async () => {
+    const userRef = doc(db, "users", "R3bBHMoUqGYN0g79TA3aoVWYMIn1");
+    
+    try {
+        const userSnap = await getDoc(userRef);
+        console.log(userSnap);
+        console.log('User data:', userSnap.data());
+    
+    } catch(err) {
+        console.log(err);
+    }
+    
+
+    
+}
