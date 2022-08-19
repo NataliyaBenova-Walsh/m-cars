e26b5e6a57ee87e8adbdb29639e3b03cf5a432b7
NataliyaBenova-Walsh <110407899+NataliyaBenova-Walsh@users.noreply.github.com>
2022-08-19 19:35:13 +0200

    Responsive CSS

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { auth, db, logout } from '../../firebase-config';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 
 
 export const Header = () => {
@@ -14,44 +15,65 @@ export const Header = () => {
 		await logout();
 		navigate('/');
 	}
+
+	const [isNavExpanded, setIsNavExpanded] = useState(false);
     return (
               
-<div id="wrapper">
+
 	<section id="header-wrapper">
-		<div id="header" className="container">
-			<div id="logo">
-				<h1><Link to="/">M CARS</Link></h1>
-			</div>
-			<nav className="menu">
-					<label for="hamburger">&#9776;</label>
-  					<input type="checkbox" id="hamburger"/>
-				<ul>
-					{user && <li className='welcomeUser'>Welcome {user.email}</li>}
-					<li><Link to="/catalog">Available cars</Link></li>
-					{ user ? 
+
+		<div id="header">
+		<nav className='nav-menu'>
+					<div id="logo">
+						<h1 id="logo-title"><Link className='logo-link' to="/">M CARS</Link></h1>
+					</div>
+
+					<button 
+						className='hamburger'
+						onClick={()=> {
+							setIsNavExpanded(!isNavExpanded);
+						}}>
+							{isNavExpanded 
+							? <FontAwesomeIcon icon={faXmark} inverse className='icon-menu icon-x'/>
+							: <FontAwesomeIcon icon={faBars} inverse className='icon-menu icon-bars'/>}
+						
+						
+					</button>
+
+			<div className={
+				isNavExpanded ? "menu expanded" : "menu"
+			}>
+				
+			
+					
+					<ul className='nav-ul'>
+						{user && <li className='welcomeUser'>Welcome {user.email}</li>}
+						<li><Link to="/catalog">Available cars</Link></li>
+						{ user ? 
 						<div className='user'>
 						
-						<li><Link to="/create">Create Offer</Link></li>
-						<li><button className='logoutBtn' onClick={onLogout}>Logout</button></li>
-					</div> 
-						:
+							<li><Link to="/create">Create Offer</Link></li>
+							<li><button className='logoutBtn' onClick={onLogout}>Logout</button></li>
+						</div> 
+							:
 						<div className='guest'>
-						<li><Link to="/login">Login</Link></li>
-                    	<li><Link to="/Register">Register</Link></li>
+							<li><Link to="/login">Login</Link></li>
+                    		<li><Link to="/Register">Register</Link></li>
 					</div>
 					}
                     
-				</ul>
+					</ul>
+					</div>
 			</nav>
-			
+				
   					
-  	
+			
 
 		</div>
 	</section>
 	
 	
 	
-</div>
+
     );
 }
